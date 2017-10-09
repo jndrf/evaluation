704c1a7ea1ceb767817a35119b75748abb2d09d3
Jonas Neundorf <jneun1@web.de>
2017-10-09 12:22:20 +0200

    added second ratio plot

--- spread_comparison.C
+++ spread_comparison.C
@@ -7,6 +7,8 @@ void spread_comparison(TString component = "Higgsstrahlung", double xsBR = 500)
   cv.Clear();
   cv.Divide(2, 2);
 
+  TString cuts = tools::cutstring(2);
+
   // Read in files
 
   TFile dat_smearscale(tools::basepath+"/CMS_smearscale/"+component+"/heppy.analyzers.examples.zh_fourjet.TreeProducer.TreeProducer_1/tree.root");
@@ -19,11 +21,8 @@ void spread_comparison(TString component = "Higgsstrahlung", double xsBR = 500)
 
   TFile dat_gensmear(tools::basepath+"/CMS_gensmear/"+component+"/heppy.analyzers.examples.zh_fourjet.TreeProducer.TreeProducer_1/tree.root");
   TTree *tree_gensmear;
-  dat_pure.GetObject("events", tree_pure);
+  dat_pure.GetObject("events", tree_gensmear);
   
-
-  TString cuts = tools::cutstring(2);
-
   // overlay plot and ratio
   {
     cv.cd(1);
@@ -57,33 +56,41 @@ void spread_comparison(TString component = "Higgsstrahlung", double xsBR = 500)
     TH1D ratio = smearscale;
     ratio.Divide(&gensmear);
     ratio.SetTitle("heppy_BES over PYTHIA_BES");
+    ratio.Fit("[0]");
     ratio.DrawClone();
-      
+
+    cv.cd(3);
+
+    ratio = smearscale;
+    ratio.Divide(&pure);
+    ratio.SetTitle("heppy BES over no BES");
+    ratio.Fit("pol(0)");
+    ratio.DrawClone();
 
   }
 
   // vismass
   {
-    cv.cd(3);
+    cv.cd(4);
 
-    TH1D smearscale = tools::makeHisto("smearscale", tree_smearscale, xsBR, 0, cuts, "vismass");
-    TH1D pure = tools::makeHisto("pure", tree_pure, xsBR, 0, cuts, "vismass");
-    TH1D gensmear = tools::makeHisto("gensmear", tree_gensmear, xsBR, 0, cuts, "vismass");
+    TH1D smearscale2 = tools::makeHisto("smearscale", tree_smearscale, xsBR, 0, cuts, "vismass");
+    TH1D pure2 = tools::makeHisto("pure", tree_pure, xsBR, 0, cuts, "vismass");
+    TH1D gensmear2 = tools::makeHisto("gensmear", tree_gensmear, xsBR, 0, cuts, "vismass");
 
     // set colors
-    pure.SetLineColor(kBlue);
-    gensmear.SetLineColor(kGreen);
-    smearscale.SetLineColor(kRed);
+    pure2.SetLineColor(kBlue);
+    gensmear2.SetLineColor(kGreen);
+    smearscale2.SetLineColor(kRed);
   
-    pure.DrawClone();
-    gensmear.DrawClone("same");
-    smearscale.DrawClone("same");
+    pure2.DrawClone();
+    gensmear2.DrawClone("same");
+    smearscale2.DrawClone("same");
 
     // legend
     TLegend leg(.15, .7, .35, .9);
-    leg.AddEntry(&pure, "no BES", "l");
-    leg.AddEntry(&smearscale, "heppy BES", "l");
-    leg.AddEntry(&gensmear, "PYTHIA BES", "l");
+    leg.AddEntry(&pure2, "no BES", "l");
+    leg.AddEntry(&smearscale2, "heppy BES", "l");
+    leg.AddEntry(&gensmear2, "PYTHIA BES", "l");
 
     leg.DrawClone();
 
